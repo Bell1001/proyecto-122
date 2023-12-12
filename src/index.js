346bfb17b464db47623d660133970b77f71e3c15
TaniaColqueCarlo <tcolquec@fcpn.edu.bo>
2023-12-12 17:35:34 -0400

    mejroas en el codigo 2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom'
 
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
